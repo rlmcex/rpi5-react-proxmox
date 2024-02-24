34413864efb5d5f045c2cf71fba348760927797b
rlmcex <93286613+rlmcex@users.noreply.github.com>
2024-02-24 15:24:33 -0800

    se cargo la funcion que manejara los resultados

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import './App.css';
+import { useState } from 'react';
+// import { evaluate } from 'mathjs';
 
 function App() {
+
+  const [input, setInput] = useState('');
+
+  const agregarInput = val => {
+    setInput(input + val);
+  };
+
+  // const calcularResultado = () => {
+  //   if (input) {
+  //     setInput(evaluate(input));
+  //   } else {
+  //     alert("Por favor ingrese valores para realizar los cálculos.");
+  //   }
+  // };
+
   return (
     <div className="App">
       Connected:  ssh from Mac to proxmox Raspberry Pi 5 1TB 8GB.
